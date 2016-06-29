c54de5f4a451ceef117faa7d3f6f862511a2a9cb
superxi911 <superxi@aliyun.com>
2016-06-29 11:01:45 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+From busybox
+
+COPY project-b /
+
+CMD ["./project-b"]
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	lib "lib-a"
+)
+
+func main() {
+	fmt.Println("This is project b")
+	lib.Print()
+}
